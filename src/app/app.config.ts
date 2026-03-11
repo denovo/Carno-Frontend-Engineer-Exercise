c6799a016c941d29c1bc996fd476834bfebecdce
denovo <pete.richardson82@gmail.com>
2026-03-11 15:43:08 +0000

    feat(02-06): wire provideStore, provideEffects, provideStoreDevtools into app.config.ts
    - Register provideStore({ [tasksFeatureKey]: tasksReducer })
    - Register provideEffects([TaskEffects])
    - Register provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode(), autoPause: true })

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { provideStore } from "@ngrx/store";
+import { provideEffects } from "@ngrx/effects";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 import { routes } from "./app.routes";
+import { tasksReducer, tasksFeatureKey } from "@app/core/store/reducers/task.reducer";
+import { TaskEffects } from "@app/core/store/effects/task.effects";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideStore({ [tasksFeatureKey]: tasksReducer }),
+    provideEffects([TaskEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+    }),
+  ],
 };
